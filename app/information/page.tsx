e9a61b2f8dd46aea0cb0fab8bf65a4231c3709b4
bodeby <thorbensen@gmail.com>
2025-07-04 00:27:21 +0200

    feat[contact]: added contact form

--- app/information/page.tsx
+++ app/information/page.tsx
@@ -9,27 +9,6 @@ export default function Kontakt() {
     return (
         <div className="w-full">
 
-            {/** Location */}
-            <section className="mx-auto max-w-screen-xl px-4 py-8 sm:px-6 lg:px-8">
-                <p>CosmosGruppen er beliggende på adressen: Klostergårdsvej 9, 8882 Fårvang</p>
-
-                <p>Vi ligger midt i hjertet af det midtjyske - i fredelige, naturskønne omgivelser med egen skov og fiskesøer lige uden for døren.</p>
-            </section>
-
-            <section className="mx-auto max-w-screen-xl px-4 py-8 sm:px-6 lg:px-8 h-128">
-                <iframe
-                    className="contact-map"
-                    title="map"
-                    scrolling="no"
-                    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2648.921164217193!2d9.687420612702184!3d56.3008452495348!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x464bf626d167b7c1%3A0x2bff902100d5561f!2sKlosterg%C3%A5rdsvej%209%2C%208882%20F%C3%A5rvang!5e1!3m2!1sen!2sdk!4v1751571904726!5m2!1sen!2sdk"
-                    width="100%"
-                    height="100%"
-                    loading="lazy"
-                    referrerPolicy="no-referrer-when-downgrade"
-                    frameBorder="0">
-                </iframe>
-            </section>
-
             {/* Personel */}
             <section>
                 <div className="mx-auto max-w-screen-xl px-4 py-8 sm:px-6 lg:px-8">
@@ -130,6 +109,79 @@ export default function Kontakt() {
                                 </p>
                             </div>
                         </div>
+                    </div>
+                </div>
+            </section>
+
+            {/** Location */}
+            <section className="mx-auto max-w-screen-xl px-4 py-8 sm:px-6 lg:px-8">
+                <p>CosmosGruppen er beliggende på adressen: Klostergårdsvej 9, 8882 Fårvang</p>
+
+                <p>Vi ligger midt i hjertet af det midtjyske - i fredelige, naturskønne omgivelser med egen skov og fiskesøer lige uden for døren.</p>
+            </section>
+
+            <section className="mx-auto max-w-screen-xl px-4 py-8 sm:px-6 lg:px-8 h-128">
+                <iframe
+                    className="contact-map"
+                    title="map"
+                    scrolling="no"
+                    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2648.921164217193!2d9.687420612702184!3d56.3008452495348!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x464bf626d167b7c1%3A0x2bff902100d5561f!2sKlosterg%C3%A5rdsvej%209%2C%208882%20F%C3%A5rvang!5e1!3m2!1sen!2sdk!4v1751571904726!5m2!1sen!2sdk"
+                    width="100%"
+                    height="100%"
+                    loading="lazy"
+                    referrerPolicy="no-referrer-when-downgrade"
+                    frameBorder="0">
+                </iframe>
+            </section>
+
+            <section className="bg-gray-50">
+                <div className="p-8 md:p-12 lg:px-16 lg:py-24">
+                    <div className="mx-auto max-w-lg text-center">
+                        <h2 className="text-2xl font-bold text-gray-900 md:text-3xl">
+                            Lorem, ipsum dolor sit amet consectetur adipisicing elit
+                        </h2>
+
+                        <p className="hidden text-gray-500 sm:mt-4 sm:block">
+                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quae dolor officia blanditiis
+                            repellat in, vero, aperiam porro ipsum laboriosam consequuntur exercitationem incidunt
+                            tempora nisi?
+                        </p>
+                    </div>
+
+                    <div className="mx-auto mt-8 max-w-xl">
+                        <form action="#" className="sm:flex sm:gap-4">
+                            <div className="sm:flex-1">
+                                <label htmlFor="email" className="sr-only">Email</label>
+
+                                <input
+                                    type="email"
+                                    placeholder="Email address"
+                                    className="w-full rounded-md border-gray-200 bg-white p-3 text-gray-700 shadow-xs transition focus:border-white focus:ring-3 focus:ring-yellow-400 focus:outline-hidden"
+                                />
+                            </div>
+
+                            <button
+                                type="submit"
+                                className="group mt-4 flex w-full items-center justify-center gap-2 rounded-md bg-rose-600 px-5 py-3 text-white transition focus:ring-3 focus:ring-yellow-400 focus:outline-hidden sm:mt-0 sm:w-auto"
+                            >
+                                <span className="text-sm font-medium"> Sign Up </span>
+
+                                <svg
+                                    className="size-5 shadow-sm rtl:rotate-180"
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    fill="none"
+                                    viewBox="0 0 24 24"
+                                    stroke="currentColor"
+                                >
+                                    <path
+                                        strokeLinecap="round"
+                                        strokeLinejoin="round"
+                                        strokeWidth="2"
+                                        d="M17 8l4 4m0 0l-4 4m4-4H3"
+                                    />
+                                </svg>
+                            </button>
+                        </form>
                     </div>
                 </div>
             </section>
